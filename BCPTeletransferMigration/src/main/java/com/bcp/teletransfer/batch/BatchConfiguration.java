17ec08e37885af032913680df08539a2a5c2660d
Jose Peralta Nouchi <joseperaltan@bcp.com.pe>
2018-07-05 18:57:17 -0500

    prueba item processor

--- BCPTeletransferMigration/src/main/java/com/bcp/teletransfer/batch/BatchConfiguration.java
+++ BCPTeletransferMigration/src/main/java/com/bcp/teletransfer/batch/BatchConfiguration.java
@@ -1,6 +1,11 @@
 package com.bcp.teletransfer.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -11,6 +16,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import com.bcp.teletransfer.entity.Usuario;
+import com.primeur.bcp.teletransfer.common.model.Company;
+
 
 @Configuration
 @EnableBatchProcessing
@@ -20,7 +27,7 @@ public class BatchConfiguration {
     @Bean
     public ItemReader<Usuario> reader() {
         FlatFileItemReader<Usuario> reader = new FlatFileItemReader<Usuario>();
-        reader.setResource(new ClassPathResource("tt-usuario.csv"));
+        reader.setResource(new ClassPathResource("tt_usuario_test.csv"));
         reader.setLineMapper(new DefaultLineMapper<Usuario>() {{
             setLineTokenizer(new DelimitedLineTokenizer() {{
                 setNames(new String[] {"chrCodigoUsuario","vchNombreUsuario","chrTipoDocIdentidad","chrDocumento","vchResponsable","vchTelefono","vchFax","vchDireccion","vchEMail","vchDepartamento","vchProvincia","vchDistrito","vchFrecuenciaUso","intVolumenPromedio"});
@@ -30,6 +37,30 @@ public class BatchConfiguration {
             }});
         }});
         return reader;
+    }
+    
+    @Bean
+    public ItemProcessor<Usuario, Company> processor() {
+        return new EmpresaItemProcessor();
+    }
+    
+    
+    @Bean
+    public Step step(StepBuilderFactory stepBuilderFactory, ItemReader<Usuario> reader, ItemProcessor<Usuario, Company> processor) {
+        return stepBuilderFactory.get("step")
+                .<Usuario, Company> chunk(3)
+                .reader(reader)
+                .processor(processor)
+//                .writer(writer)
+                .build();
+    }
+    
+    @Bean
+    public Job createCompany(JobBuilderFactory jobs, Step step) {
+        return jobs.get("createCompany")
+                .flow(step)
+                .end()
+                .build();
     }
 
 }
